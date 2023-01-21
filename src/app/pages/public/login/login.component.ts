577a0659bf069e3b93747ddc9a9d0d9d562e0ae2
Sebas15897 <sebastiancontreras15897@gmail.com>
2023-01-21 14:23:37 -0500

    feat: create form login

--- src/app/pages/public/login/login.component.ts
+++ src/app/pages/public/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
+  hide: boolean = true;
+  formLogin: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.formLogin = this.createForm();
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  createForm(): FormGroup {
+    return this.fb.group({
+      email: [null, Validators.required],
+      password: [null, Validators.required],
+      remindme: [false]
+    })
+  }
+
+  get invalidForm(): boolean {
+    return this.formLogin.invalid;
   }
 
 }
